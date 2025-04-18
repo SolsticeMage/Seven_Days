091dd9cac10118569161691e3393a057d84e5df9
SolsticeMage <solsticemage@mage-tower.lan>
2025-04-17 19:11:40 -0600

    My attempt at calculating vertex normals

--- Testing/shield_mesh.gd
+++ Testing/shield_mesh.gd
@@ -8,23 +8,23 @@
 @tool
 extends MeshInstance3D
 
+var mesh_data = [] #declares mesh_data as array (empty)
 
 func _ready():
-	var mesh_data = [] #declares mesh_data as array (empty)
 	mesh_data.resize(ArrayMesh.ARRAY_MAX) #Resize for Mesh.ArrayType data (enum)
 	#All vertex points in the mesh
 	mesh_data[ArrayMesh.ARRAY_VERTEX] = PackedVector3Array(
 		[
-			Vector3(.6,.6,0),
-			Vector3(.6,.6,.3),
-			Vector3(-.6,.6,.3),
-			Vector3(-.6,.6,0),
-			Vector3(-.3,-.6,0),
-			Vector3(0,-.9,0),
-			Vector3(.3,-.6,0),
-			Vector3(.3,-.6,.3),
-			Vector3(0,-.9,.3),
-			Vector3(-.3,-.6,.3)
+			Vector3(.6,.6,0),	#0
+			Vector3(.6,.6,.3),	#1
+			Vector3(-.6,.6,.3),	#2
+			Vector3(-.6,.6,0),	#3
+			Vector3(-.3,-.6,0),	#4
+			Vector3(0,-.9,0),	#5
+			Vector3(.3,-.6,0),	#6
+			Vector3(.3,-.6,.3),	#7
+			Vector3(0,-.9,.3),	#8
+			Vector3(-.3,-.6,.3)	#9
 		]
 	)
 	# Selects vertices by index for drawing triangles (3 verts per triangle)
@@ -35,5 +35,50 @@ func _ready():
 			4,9,8, 2,9,3, 3,9,4, 1,7,9, 1,9,2, 9,7,8
 		]
 	)
+	# If the vertex is part of multiple faces then its normal should be the average of the faces
+	#	note: THERE MUST BE A NORMAL FOR EVERY VERTEX otherwise 'add_surface_from_arrays()' breaks.
+	#TODO: Fix the test_normal for the love of god
+	var test_normal = get_vector3_average(PackedVector3Array(
+		[
+			get_triangle_normal(0,1,2),
+			get_triangle_normal(0,2,3),
+			get_triangle_normal(0,3,4),
+			get_triangle_normal(0,4,6),
+			get_triangle_normal(0,6,1)
+		]
+	))
+	print(test_normal)
+	mesh_data[ArrayMesh.ARRAY_NORMAL] = PackedVector3Array(
+		[
+			test_normal,
+			Vector3.ZERO,
+			Vector3.ZERO,
+			Vector3.ZERO,
+			Vector3.ZERO,
+			Vector3.ZERO,
+			Vector3.ZERO,
+			Vector3.ZERO,
+			Vector3.ZERO,
+			Vector3.ZERO
+		]
+	)
 	mesh = ArrayMesh.new()
 	mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, mesh_data)
+
+func get_vertex(i):
+	return mesh_data[ArrayMesh.ARRAY_VERTEX][i]
+
+#Inspiration: Godot Docs -> Math -> Vector Math -> Cross Product -> Calculating Normals
+#Link: https://docs.godotengine.org/en/stable/tutorials/math/vector_math.html#calculating-normals
+##Takes three indices in Array_Vertex and returns the facing direction of the resulting triangle
+func get_triangle_normal(a:int, b:int, c:int):
+	var side1 = get_vertex(b) - get_vertex(a)
+	var side2 = get_vertex(c) - get_vertex(a)
+	var normal = side1.cross(side2) #Cross product is perpendicular to the surface (normal)
+	return normal
+	
+func get_vector3_average(vectors: PackedVector3Array):
+	var sum := Vector3.ZERO
+	for vector in vectors:
+		sum += vector
+	return Vector3(sum / vectors.size())
