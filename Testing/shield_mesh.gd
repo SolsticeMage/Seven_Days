f805a9b1088cd4c0d65d3737a7a809ae1740933d
SolsticeMage <solsticemage@mage-tower.lan>
2025-04-23 18:58:08 -0600

    Finished all normal calculations. Will probably simplify further but first I'm investigating an issue with the lighting looking choppy on the mesh

--- Testing/shield_mesh.gd
+++ Testing/shield_mesh.gd
@@ -31,7 +31,7 @@ func _ready():
 		Vector3(-.3,-.6,.3),#9
 	])
 	# Selects vertices by index for drawing triangles (3 verts per triangle)
-	#	Triangle's visible face is the side with vertices in clockwise order
+	#	The visible face is the side where its vertices are in clockwise order
 	mesh_data[ArrayMesh.ARRAY_INDEX] = PackedInt32Array([
 		0,1,2, 0,2,3, 0,3,4, 0,4,6, 4,5,6, 0,6,1, 1,6,7, 5,7,6,
 		5,8,7, 4,8,5, 4,9,8, 2,9,3, 3,9,4, 1,7,9, 1,9,2, 9,7,8,
@@ -66,41 +66,47 @@ func _ready():
 	])
 	mesh = ArrayMesh.new()
 	mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, mesh_data)
+	for i in mesh_data[ArrayMesh.ARRAY_VERTEX].size():
+		print("#", i, ": ")
+		print("Vertex: ", mesh_data[ArrayMesh.ARRAY_VERTEX][i])
+		print("Normal: ", mesh_data[ArrayMesh.ARRAY_NORMAL][i], "\n")
 
 ## Returns the normal from the mesh data for the given vertex
 func get_vertex_normal(vertex:int):
 	var face_normals = PackedVector3Array()
-	var array_index = mesh_data[ArrayMesh.ARRAY_INDEX]
-	var array_vertex = mesh_data[ArrayMesh.ARRAY_VERTEX]
+	var indices = mesh_data[ArrayMesh.ARRAY_INDEX]
+	var vertices = mesh_data[ArrayMesh.ARRAY_VERTEX]
 	
 	#TODO: You fucked up again. Find out where, and fix.
-	# Iterate through array_index to find the provided vertex
-	for i in array_index.size():
-		if array_index[i] == vertex:
-			# Find the first vertex in the triangle (set of 3) it belongs to
-			var first_tri = i - i % 3
+	# Iterate through indices to find the provided vertex
+	for i in indices.size():
+		if indices[i] == vertex:
+			# Find the index of the first vertex in its triangle (set of 3)
+			var first_triangle_vert = i - i % 3
 			# Append the normal of the triangle face
-			face_normals.append(get_face_normal(
-					array_vertex[first_tri],
-					array_vertex[first_tri+1],
-					array_vertex[first_tri+2]))
+			face_normals.append(
+					get_face_normal(
+					vertices[indices[first_triangle_vert]],
+					vertices[indices[first_triangle_vert+1]],
+					vertices[indices[first_triangle_vert+2]])
+					)
 			# Jump to the first vertex in the next triangle
-			if first_tri+3 < array_index.size():
-				i = first_tri+3
+			if first_triangle_vert+3 < indices.size():
+				i = first_triangle_vert+3
 			else: break
 	return get_normalized_average(face_normals)
 
 
 ##Takes three vectors and returns the facing direction of the resulting triangle
-func get_face_normal(a:Vector3, b:Vector3, c:Vector3):
+func get_face_normal(a:Vector3, b:Vector3, c:Vector3)->Vector3:
 	var side1 = b - a
 	var side2 = c - a
 	var normal = side2.cross(side1) #Cross product is perpendicular to surface
 	return normal.normalized()
 
 
-func get_normalized_average(vectors:PackedVector3Array):
+func get_normalized_average(vectors:PackedVector3Array)->Vector3:
 	var sum := Vector3.ZERO
 	for vector in vectors:
 		sum += vector
-	return (sum / vectors.size()).normalized()
+	return (sum / vectors.size())
